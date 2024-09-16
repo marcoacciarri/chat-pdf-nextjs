2cd656f05520b2b0a14dafbf1e5e23ca86b09368
Marco <marco.acciarri@yourzone.it>
2024-09-16 11:56:39 +0200

    add icons to fileuploader

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { RocketLaunch, ArrowCircleDown } from '@mui/icons-material'
 
 
 export const FileUploader = () => {
@@ -14,11 +15,19 @@ export const FileUploader = () => {
             className={`border-2 border-dashed border-gray-300 rounded-md p-4 h-96 flex items-center justify-center ${isFocused || isDragActive ? 'border-blue-500 bg-indigo-900' : ''}`}
         >
             <input {...getInputProps()} />
-            {isDragActive ? (
-                <p>Drop the files here ...</p>
-            ) : (
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            )}
+            <div className='flex flex-col items-center'>
+                {isDragActive ? (
+                    <>
+                        <RocketLaunch className='h- w-16 animate-ping' />
+                        <p>Drop the files here ...</p>
+                    </>
+                ) : (
+                    <>
+                        <ArrowCircleDown font-size="large" className='animate-bounce' />
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                    </>
+                )}
+            </div>
         </div >
     )
 } 
