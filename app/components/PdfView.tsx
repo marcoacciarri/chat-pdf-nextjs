de4965584a8363e5125d9b2f80ed9e13826866be
Marco Acciarri <marco.acciarri@yourzone.it>
2024-10-09 18:02:05 +0200

    add instructions to configure CORS for firebase

--- app/components/PdfView.tsx
+++ app/components/PdfView.tsx
@@ -7,6 +7,13 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 
+// We need to configure cors
+//go to https://console.cloud.google.com/
+//click on Active Cloud Shell
+//create a new file in editor called cors.json
+//run gsutil cors set cors.json gs://chat-pdf-nextjs.appspot.com
+//app-name is the name of the app in Firebase
+//https://firebase.google.com/docs/storage/web/download-files#cors_configuration
 
 interface Props {
     url: string
